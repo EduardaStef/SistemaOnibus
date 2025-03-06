fac40e57c50d69dc16918bebd4a97ad0332e02de
eduardastef <eduarda.steffen@hotmail.com>
2025-03-06 20:00:22 -0300

    teste

--- src/com/furb/view/TelaConsultaViagens.java
+++ src/com/furb/view/TelaConsultaViagens.java
@@ -5,7 +5,11 @@ DATA: 25/02/2025
 
 package com.furb.view;
 
-public class TelaConsultaViagens {
+import javax.swing.*;
+
+public class TelaConsultaViagens extends Tela{
+
+    private JFrame telaConsultaViagem;
 
     /* TO DO:
     Criar tela de consulta de viagens que envie para a tela de cadastro de passageiros e de consulta de passageiros
